3c96a3cb81754803a823c6f5ae90c1acd074cc5d
LORD TAWA THE GREAT <tawajones1488@outlook.com>
2026-03-03 14:39:06 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fadeSections = document.querySelectorAll('.fade-section');
+  const hero = document.querySelector(".hero");
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".site-nav__desktop");
 
   const appearOptions = {
     threshold: 0.2,
   };
-	
-document.addEventListener("DOMContentLoaded", () => {
-  const hero = document.querySelector(".hero");
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".site-nav__desktop");
 
   if (hamburger && nav) {
     hamburger.addEventListener("click", (e) => {
@@ -23,32 +21,28 @@ document.addEventListener("DOMContentLoaded", () => {
         hamburger.classList.remove("active");
       }
     });
-
   } else {
     console.info("LOL! THIS CODE IS BROKEN(hamburger:", !!hamburger, "nav:", !!nav, ")");
   }
 
   let lastScrollY = window.scrollY || 0;
-
   const heroHeight = hero ? hero.offsetHeight : 0;
   const heroBottom = Math.max(heroHeight * 0.8, 0);
-
-
   let ticking = false;
+
   window.addEventListener("scroll", () => {
     if (!ticking) {
       window.requestAnimationFrame(() => {
         const currentScroll = window.scrollY || 0;
-		
-
         lastScrollY = currentScroll;
         ticking = false;
       });
       ticking = true;
     }
   });
+
   window.addEventListener("resize", () => {
-    const desktopBreakpoint = 900; // 
+    const desktopBreakpoint = 900;
     if (window.innerWidth > desktopBreakpoint) {
       nav && nav.classList.remove("active");
       hamburger && hamburger.classList.remove("active");
